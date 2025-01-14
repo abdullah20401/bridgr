135f62bbef010d65d456486cee0bcfc44c554673
abdullah20401 <siddiquiabdullah2017@gmail.com>
2025-01-13 21:04:13 -0600

    Implement routing with Login and Home components; update package-lock.json

---
+++ bridgr_frontend/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import {FcGoogle} from 'react-icons/fc'
+
+function Login() {
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>Login</div>
+  )
+}
+
+export default Login
