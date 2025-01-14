eeaed784510e59d56f61a4dd39bbcbe16554b0e5
abdullah20401 <siddiquiabdullah2017@gmail.com>
2025-01-14 02:27:56 -0600

    Add Google OAuth login functionality and update .gitignore; include dotenv and jwt-decode dependencies

--- bridgr_frontend/vite.config.js
+++ bridgr_frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env.REACT_APP_GOOGLE_API_TOKEN": JSON.stringify(
+      process.env.REACT_APP_GOOGLE_API_TOKEN
+    ),
+  },
+});
